a740fbdd4b19a02e9e78fdc61bdfb286cea95518
timotheeGuille <timothee.guillemaille@ik.me>
2021-09-23 18:58:34 +0200

    error

--- C DCGAN/C_DC_GAN.py
+++ C DCGAN/C_DC_GAN.py
@@ -208,7 +208,7 @@ def train(dataset, epochs):
   
 def generate_and_save_images(model, epoch, test_input):
 
-  predictions = model([test_input,[0,1,2,3,4,5,6,7,8,9,1,2,3,4,5,6]], training=False)
+  predictions = model(([test_input],np.array([0,1,2,3,4,5,6,7,8,9,1,2,3,4,5,6])), training=False)
 
   fig = plt.figure(figsize=(4, 4))
 
