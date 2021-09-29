9181d5e8db2df9192b99fa6a57625cb0186b45ac
timotheeGuille <timothee.guillemaille@ik.me>
2021-09-29 11:43:29 +0200

    test G loss -1 remove

--- WDCGAN/WDCGAN.py
+++ WDCGAN/WDCGAN.py
@@ -136,24 +136,26 @@ def gradient_penalty(images, generated_images):
     gp=tf.reduce_mean( ( norme - 1.0 ) ** 2 )
     return gp
 
-def discriminator_loss2(real_output, fake_output,gp):
+def discriminator_loss(real_output, fake_output,gp):
     coeff = 10.0
     loss= (tf.reduce_mean(real_output)-tf.reduce_mean(fake_output) + coeff * gp)
     return loss
 
-def generator_loss2(fake_output):
-    loss=  (-1)*tf.reduce_mean(fake_output)
+def generator_loss(fake_output):
+    loss=  tf.reduce_mean(fake_output)
     return loss
 
 
 cross_entropy = tf.keras.losses.BinaryCrossentropy(from_logits=True)
-def discriminator_loss(real_output, fake_output,gp):
+
+
+def discriminator_loss2(real_output, fake_output,gp):
     real_loss = cross_entropy(tf.ones_like(real_output), real_output)
     fake_loss = cross_entropy(tf.zeros_like(fake_output), fake_output)
     total_loss = real_loss + fake_loss
     return total_loss
 
-def generator_loss(fake_output):
+def generator_loss2(fake_output):
     return cross_entropy(tf.ones_like(fake_output), fake_output)
 
 
