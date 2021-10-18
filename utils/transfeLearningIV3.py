85953753654aaff04a5f772b8635a542cf46c053
timothee10 <72388955+timothee10@users.noreply.github.com>
2021-10-18 02:26:29 +0200

    trqnsfert l

--- utils/transfeLearningIV3.py
+++ utils/transfeLearningIV3.py
@@ -1,40 +1,153 @@
 
+import os
+import zipfile
 import tensorflow as tf
-
-import matplotlib.pyplot as plt
-import numpy as np
+from tensorflow.keras.optimizers import RMSprop
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras import layers
-
 from tensorflow.keras import Model
+import matplotlib.pyplot as plt
 
+BATCH_SIZE = 256
+EPOCHS = 50
+#import
+(x_train,y_train),(x_val,y_val)=tf.keras.datasets.mnist.load_data()
 
-(x_train,y_train),(_,_)=tf.keras.datasets.mnist.load_data()
+x_train = tf.expand_dims(x_train, axis=-1)
+x_train = tf.image.grayscale_to_rgb(x_train)
 
-BATCH_SIZE=256
+x_val = tf.expand_dims(x_val, axis=-1)
+x_val = tf.image.grayscale_to_rgb(x_val)
 
+x_train = tf.image.resize(x_train, (252,252), method='nearest')
+x_val = tf.image.resize(x_val, (252,252), method='nearest')
 
-#cut size to avoid size bigger than batchsize
-size_cut=x_train.shape[0]-x_train.shape[0]%BATCH_SIZE
-
-x_train=x_train[0:size_cut,:,:]
-y_train=y_train[0:size_cut]
-
-#reshape and norm
-x_train = x_train.reshape(x_train.shape[0], 28, 28, 1).astype('float32')
-x_train= ( x_train - 127.5 ) / 127.5
+############add norm
 
 #suffle
 train_dataset = tf.data.Dataset.from_tensor_slices(x_train).shuffle(x_train.shape[0]).batch(BATCH_SIZE)
 
 
-
 from tensorflow.keras.applications.inception_v3 import InceptionV3
 
-pre_trained_model = InceptionV3(input_shape = (28, 28, 1), # Shape of our images
+pre_trained_model = InceptionV3(input_shape = (252, 252, 3), # Shape of our images
                                 include_top = False, # Leave out the last fully connected layer
                                 weights = 'imagenet')
 
+                                
+for layer in pre_trained_model.layers:
+  layer.trainable = False
 
+from tensorflow.keras.optimizers import RMSprop
+
+# Flatten the output layer to 1 dimension
+x = layers.Flatten()(pre_trained_model.output)
+# Add a fully connected layer with 1,024 hidden units and ReLU activation
+x = layers.Dense(128, activation='relu')(x)
+# Add a dropout rate of 0.2
+x = layers.Dropout(0.2)(x)                  
+# Add a final sigmoid layer for classification
+x = layers.Dense  (10, activation='sigmoid')(x)           
+
+model = Model( pre_trained_model.input, x) 
+
+model.compile(optimizer = 'adam', 
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True), 
+              metrics = ['acc'])   
+
+model.summary()
+
+history = model.fit(
+            x_train,
+            y_train ,
+            batch_size=BATCH_SIZE,
+            epochs = 100,
+            validation_data = (x_val,y_val),
+            verbose = 1)
+
+model.save('my_model')
+
+import matplotlib.pyplot as plt
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs = range(len(acc))
+
+plt.plot(epochs, acc, 'bo', label='Training accuracy')
+plt.plot(epochs, val_acc, 'b', label='Validation accuracy')
+plt.title('Training and validation accuracy')
+
+plt.figure()
+
+plt.plot(epochs, loss, 'bo', label='Training Loss')
+plt.plot(epochs, val_loss, 'b', label='Validation Loss')
+plt.title('Training and validation loss')
+plt.legend()
+
+## *****************************************************************************************************************************************************************
+
+"""
+local_zip = 'utils/tmp/cats_and_dogs_filtered.zip'
+zip_ref = zipfile.ZipFile(local_zip, 'r')
+zip_ref.extractall('tmp')
+zip_ref.close()
+
+base_dir = 'utils/tmp/cats_and_dogs_filtered'
+train_dir = os.path.join(base_dir, 'train')
+validation_dir = os.path.join(base_dir, 'validation')
+
+# Directory with our training cat pictures
+train_cats_dir = os.path.join(train_dir, 'cats')
+
+# Directory with our training dog pictures
+train_dogs_dir = os.path.join(train_dir, 'dogs')
+
+# Directory with our validation cat pictures
+validation_cats_dir = os.path.join(validation_dir, 'cats')
+
+# Directory with our validation dog pictures
+validation_dogs_dir = os.path.join(validation_dir, 'dogs')
+
+# Set up matplotlib fig, and size it to fit 4x4 pics
+import matplotlib.image as mpimg
+nrows = 4
+ncols = 4
+
+fig = plt.gcf()
+fig.set_size_inches(ncols*4, nrows*4)
+pic_index = 100
+train_cat_fnames = os.listdir( train_cats_dir )
+train_dog_fnames = os.listdir( train_dogs_dir )
+
+
+next_cat_pix = [os.path.join(train_cats_dir, fname) 
+                for fname in train_cat_fnames[ pic_index-8:pic_index] 
+               ]
+
+next_dog_pix = [os.path.join(train_dogs_dir, fname) 
+                for fname in train_dog_fnames[ pic_index-8:pic_index]
+               ]
+
+for i, img_path in enumerate(next_cat_pix+next_dog_pix):
+  # Set up subplot; subplot indices start at 1
+  sp = plt.subplot(nrows, ncols, i + 1)
+  sp.axis('Off') # Don't show axes (or gridlines)
+
+  img = mpimg.imread(img_path)
+  plt.imshow(img)
+
+plt.show()
+
+# Import the inception model  
+from tensorflow.keras.applications.inception_v3 import InceptionV3
+
+pre_trained_model = InceptionV3(input_shape = (150, 150, 3), # Shape of our images
+                                include_top = False, # Leave out the last fully connected layer
+                                weights = 'imagenet')
+
+                                
 for layer in pre_trained_model.layers:
   layer.trainable = False
 
@@ -43,7 +156,7 @@ class myCallback(tf.keras.callbacks.Callback):
   def on_epoch_end(self, epoch, logs={}):
     if(logs.get('acc')>0.959):
       print("\nReached 99.9% accuracy so cancelling training!")
-      self.model.stop_training = True
+      self.model.stop_training = True  
 
 
 from tensorflow.keras.optimizers import RMSprop
@@ -61,4 +174,67 @@ model = Model( pre_trained_model.input, x)
 
 model.compile(optimizer = RMSprop(lr=0.0001), 
               loss = 'binary_crossentropy', 
-              metrics = ['acc'])
+              metrics = ['acc'])      
+
+
+# Add our data-augmentation parameters to ImageDataGenerator
+train_datagen = ImageDataGenerator(rescale = 1./255.,
+                                   rotation_range = 40,
+                                   width_shift_range = 0.2,
+                                   height_shift_range = 0.2,
+                                   shear_range = 0.2,
+                                   zoom_range = 0.2,
+                                   horizontal_flip = True)
+
+# Note that the validation data should not be augmented!
+test_datagen = ImageDataGenerator( rescale = 1.0/255. )
+
+# Flow training images in batches of 20 using train_datagen generator
+train_generator = train_datagen.flow_from_directory(train_dir,
+                                                    batch_size = 20,
+                                                    class_mode = 'binary', 
+                                                    target_size = (150, 150))     
+
+# Flow validation images in batches of 20 using test_datagen generator
+validation_generator =  test_datagen.flow_from_directory( validation_dir,
+                                                          batch_size  = 20,
+                                                          class_mode  = 'binary', 
+                                                          target_size = (150, 150))       
+
+
+
+callbacks = myCallback()
+
+
+
+history = model.fit_generator(
+            train_generator,
+            validation_data = validation_generator,
+            steps_per_epoch = 100,
+            epochs = 100,
+            validation_steps = 50,
+            verbose = 2,
+            callbacks=[callbacks])
+
+
+import matplotlib.pyplot as plt
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs = range(len(acc))
+
+plt.plot(epochs, acc, 'bo', label='Training accuracy')
+plt.plot(epochs, val_acc, 'b', label='Validation accuracy')
+plt.title('Training and validation accuracy')
+
+plt.figure()
+
+plt.plot(epochs, loss, 'bo', label='Training Loss')
+plt.plot(epochs, val_loss, 'b', label='Validation Loss')
+plt.title('Training and validation loss')
+plt.legend()
+
+plt.show()                
+"""
