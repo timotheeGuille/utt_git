59e5e1739f00a69923635c6885ac9f4082290513
timotheeGuille <timothee.guillemaille@ik.me>
2021-11-16 17:21:02 +0100

    add path

--- MSTAR/gan/gan.py
+++ MSTAR/gan/gan.py
@@ -4,7 +4,7 @@ import sys
 
 sys.path.append((os.getcwd()))
 sys.path.append(os.path.dirname(os.getcwd()))
-
+sys.path.append(os.path.dirname(os.path.dirname(os.getcwd())))
 
 import datetime
 import time
